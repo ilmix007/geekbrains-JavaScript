eed3867a3e591ca81f1a003d2043bdaea326b25e
ilmix007 <imi87@yandex,ru>
2019-09-26 23:41:18 +0500

    my homework

---
+++ lesson_01/script/name.js
@@ -0,0 +1,10 @@
+/*
+2. Работа с переменными. 
+a) Объявить две переменные: admin и name. Записать в name строку «Василий»; 
+b) Скопировать значение из name в admin. Вывести admin (должно вывести «Василий»). 
+*/
+
+var admin;
+var name = "Vasiliy";
+admin = name;
+console.log("admin = " + admin);
